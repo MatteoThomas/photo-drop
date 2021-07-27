465b2c38ed2f288b1c62cc0354741ffd324147b5
Kendra Wing <kendracwing@icloud.com>
2021-07-27 17:15:43 -0600

    Added path for homepage into App.js(root client)
    Added: Line 3-
    import home from "../src/pages/home/home";

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Profile from "../src/pages/profile/Profile.js";
+import home from "../src/pages/home/home";
 import "./App.css";
 
 class App extends Component {
