d873944c669cb37fee94ed0a1b6a4fd0cfbd7179
kaivon tolooee <kaivont@gmail.com>
2021-07-29 18:34:48 -0400

    the add/delete comments and likes

---
+++ Comment-and-likes/client/actions/actionCreators.js
@@ -0,0 +1,30 @@
+// increment
+export function increment(index) {
+    return {
+      type: 'INCREMENT_LIKES',
+      index
+    }
+  }
+  
+  // add comment
+  export function addComment(postId, author, comment) {
+    
+    return {
+      type: 'ADD_COMMENT',
+      postId,
+      author,
+      comment
+    }
+  }
+  
+  // remove comment
+  
+  export function removeComment(postId, i) {
+    return {
+      type: 'REMOVE_COMMENT',
+      i,
+      postId
+    }
+  }
+
+//   when these get fired, we need reducers to actually process it
