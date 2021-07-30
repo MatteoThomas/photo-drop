5d8fe88115128099a8c7373a0b95e6c0c3622994
Matthew Elliott <77035998+MatteoThomas@users.noreply.github.com>
2021-07-30 15:48:36 -0500

    restyling

--- client/src/pages/SearchPhotos.js
+++ client/src/pages/SearchPhotos.js
@@ -1,11 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import {
+  Container,
+  Col,
+  Form,
+  Button,
+  Card,
+  CardColumns,
+} from "react-bootstrap";
 
-import Auth from '../utils/auth';
-import { savePhotoIds, getSavedPhotoIds } from '../utils/localStorage';
-import { useMutation } from '@apollo/client';
-import { SAVE_PHOTO } from '../utils/mutations';
-import axios from "axios"
+import Auth from "../utils/auth";
+import { savePhotoIds, getSavedPhotoIds } from "../utils/localStorage";
+import { useMutation } from "@apollo/client";
+import { SAVE_PHOTO } from "../utils/mutations";
+import axios from "axios";
+
+import "./resultStyles.css";
+import Results from "./Results";
 
 const SearchPhotos = () => {
   // create state for holding returned google api data
@@ -13,9 +23,11 @@ const SearchPhotos = () => {
   const [searchResult, setResult] = useState([]);
   // create state for holding our search field data
   // const [searchInput, setSearchInput] = useState('');
-  const [photo, setPhoto] = useState("")
+  const [photo, setPhoto] = useState("");
 
-  const [clientId, setClientID] = useState("aczCYkM2vBISaUTNddlmnXpXV8_2TYD5fAzxm_23vNM")
+  const [clientId, setClientID] = useState(
+    "aczCYkM2vBISaUTNddlmnXpXV8_2TYD5fAzxm_23vNM"
+  );
 
   // create state to hold saved photoId values
   const [savedPhotoIds, setSavedPhotoIds] = useState(getSavedPhotoIds());
@@ -35,35 +47,35 @@ const SearchPhotos = () => {
     if (!photo) {
       return false;
     }
-    console.log(photo)
+    console.log(photo);
 
     try {
-      const url = "https://api.unsplash.com/search/photos?page=1&query="
-            + photo + "&client_id=" + clientId;
-      axios.get(url)
-      .then((response) => {
-        console.log(response)
-        setResult(response.data.results)
+      const url =
+        "https://api.unsplash.com/search/photos?page=1&query=" +
+        photo +
+        "&client_id=" +
+        clientId;
+      axios.get(url).then((response) => {
+        console.log(response);
+        setResult(response.data.results);
+      });
 
-    })   
-      
       // if (!url.ok) {
       //   throw new Error('something went wrong!');
       // }
-    
 
       // const { items } = await url.json();
 
       const photoData = searchResult.map((photo) => ({
         photoId: photo.id,
-        authors: photo.user.name || ['No Photographer to display'],
+        authors: photo.user.name || ["No Photographer to display"],
         title: photo.description,
-        description: photo.alt_description || ['No description'],
-        image: photo.urls.small || '',
+        description: photo.alt_description || ["No description"],
+        image: photo.urls.small || "",
       }));
 
       setResult(photoData);
-      setPhoto('');
+      setPhoto("");
     } catch (err) {
       console.error(err);
     }
@@ -85,7 +97,7 @@ const SearchPhotos = () => {
       const { data } = await savePhoto({
         variables: { photoData: { ...photoToSave } },
       });
-      console.log('data:', data);
+      console.log("data:", data);
       console.log(savedPhotoIds);
       setSavedPhotoIds([...savedPhotoIds, photoToSave.photoId]);
     } catch (err) {
@@ -95,56 +107,64 @@ const SearchPhotos = () => {
 
   return (
     <>
-      <Jumbotron fluid className='text-light bg-dark'>
-        <Container>
-          <h1>Search for Photos!</h1>
-          <Form onSubmit={handleFormSubmit}>
-            <Form.Row>
-              <Col xs={12} md={8}>
-                <Form.Control
-                  name='photo'
-                  value={photo}
-                  onChange={(event) => setPhoto(event.target.value)}
-                  type='text'
-                  size='lg'
-                  placeholder='Search for a Picture'
-                />
-              </Col>
-              <Col xs={12} md={4}>
-                <Button type='submit' variant='success' size='lg'>
-                  Submit Search
-                </Button>
-              </Col>
-            </Form.Row>
-          </Form>
-        </Container>
-      </Jumbotron>
+      {/* <Container>
+        <h1>Photo Search</h1>
+        <Form onSubmit={handleFormSubmit}>
+          <Form.Row>
+            <Col xs={12} md={8}>
+              <Form.Control
+                name="photo"
+                value={photo}
+                onChange={(event) => setPhoto(event.target.value)}
+                type="text"
+                size="lg"
+                placeholder="Search"
+              />
+            </Col>
+            <Col xs={12} md={4}>
+              <Button type="submit">Submit Search</Button>
+            </Col>
+          </Form.Row>
+        </Form>
+      </Container> */}
 
       <Container>
-        <h2>
+        {/* <h2>
           {searchResult.length
             ? `Viewing ${searchResult.length} results:`
-            : 'Search for a photo to begin'}
-        </h2>
+            : "Results"}
+        </h2> */}
+        <div className="resultContainer">
+          <Results />
+        </div>
         <CardColumns>
           {searchResult.map((photo) => {
             return (
-              <Card key={photo.id} border='dark'>
+              <Card key={photo.id} border="dark">
                 {photo.urls.small ? (
-                  <Card.Img src={photo.urls.small} alt={`The cover for ${photo.description}`} variant='top' />
+                  <Card.Img
+                    src={photo.urls.small}
+                    alt={`The cover for ${photo.description}`}
+                    variant="top"
+                  />
                 ) : null}
                 <Card.Body>
                   <Card.Title>{photo.description}</Card.Title>
-                  <p className='small'>Authors: {photo.user.username}</p>
+                  <p className="small">Authors: {photo.user.username}</p>
                   <Card.Text>{photo.description}</Card.Text>
                   {Auth.loggedIn() && (
                     <Button
-                      disabled={savedPhotoIds?.some((savedPhotoId) => savedPhotoId === photo.photoId)}
-                      className='btn-block btn-info'
-                      onClick={() => handleSavePhoto(photo.photoId)}>
-                      {savedPhotoIds?.some((savedPhotoId) => savedPhotoId === photo.photoId)
-                        ? 'This photo has already been saved!'
-                        : 'Save this photo!'}
+                      disabled={savedPhotoIds?.some(
+                        (savedPhotoId) => savedPhotoId === photo.photoId
+                      )}
+                      className="btn-block btn-info"
+                      onClick={() => handleSavePhoto(photo.photoId)}
+                    >
+                      {savedPhotoIds?.some(
+                        (savedPhotoId) => savedPhotoId === photo.photoId
+                      )
+                        ? "This photo has already been saved!"
+                        : "Save this photo!"}
                     </Button>
                   )}
                 </Card.Body>
--- client/src/pages/profile/AvatarBio.js
+++ client/src/pages/profile/AvatarBio.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import avatar1 from "./avatar1.png";
+import avatar1 from "./avatar2.png";
 class AvatarBio extends Component {
   render() {
     return (
@@ -8,16 +8,10 @@ class AvatarBio extends Component {
           <img src={avatar1} alt="avatar" />
         </div>
         <div id="text">
-          <h3>About</h3>
           <h3>
             Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil
             impedit quo minus id quod maxime placeat facere possimus, omnis
-            voluptas assumenda est, omnis dolor repellendus. Temporibus autem
-            quibusdam et aut officiis debitis aut rerum necessitatibus saepe
-            eveniet ut et voluptates repudiandae sint et molestiae non
-            recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut
-            aut reiciendis voluptatibus maiores alias consequatur aut
-            perferendis doloribus asperiores repellat.
+            voluptas assumenda est, omnis dolor repellendus.
           </h3>
         </div>
       </div>
